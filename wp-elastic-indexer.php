5e0badcbe8de57b96731b363562c4c359c3f888c
Andreas Ek <andreas@flowcom.se>
2014-10-02 14:00:21 +0200

    Initial

---
+++ wp-elastic-indexer.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: WP Elasticsearch Indexer
+Description: Post indexer example plugin for wp-elastic-api
+Author: Digital United
+Version: 0.1
+Author URI: http://www.careofhaus.io/
+*/
