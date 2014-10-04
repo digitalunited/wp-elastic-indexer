ee89783cf4b708d8d38a3e23ea549be41f110bd1
Andreas Ek <andreas@flowcom.se>
2014-10-04 12:14:23 +0200

    First index test at save_post

--- wp-elastic-indexer.php
+++ wp-elastic-indexer.php
@@ -6,3 +6,23 @@ Author: Digital United
 Version: 0.1
 Author URI: http://www.careofhaus.io/
 */
+
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
+}
+
+add_action( 'save_post', 'wp_elastic_indexer_save_post' );
+
+function wp_elastic_indexer_save_post( $post_id ) {
+
+    $data = get_object_vars( get_post( $post_id ) );
+
+    $body = array(
+        'post_type' => 'post',
+        'ID' => $post_id,
+        'data' => $data
+    );
+
+    \Httpful\Request::post( 'http://wpelastic.dev/app/plugins/wp-elastic-api/posts' )->body( json_encode( $body ) )->send();
+
+}
