02b2681f51add0afc79f220ff9a0233480d55935
Andreas Ek <andreas@flowcom.se>
2014-10-04 15:43:38 +0200

    Index saved post

--- index.php
+++ index.php
@@ -7,29 +7,51 @@ Version: 0.1
 Author URI: http://www.careofhaus.io/
 */
 
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require __DIR__ . '/vendor/autoload.php';
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require __DIR__ . '/vendor/autoload.php';
 }
 
 add_action( 'save_post', 'wp_elastic_indexer_save_post' );
 
 function wp_elastic_indexer_save_post( $post_id ) {
 
-    $data = array();
+	$api_url          = 'http://wpelastic.dev/app/plugins/wp-elastic-api';
+	$index_post_types = array(
+		'post',
+		'page'
+	);
 
-    $post = get_object_vars( get_post( $post_id ) );
-    $data = array_merge( $data, $post );
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return $post_id;
+	}
+	if ( ! isset( $_POST['post_type'] ) ) {
+		return $post_id;
+	}
 
-    $meta = get_object_vars( get_post_meta( $post_id ) );
-    $data = array_merge( $data, $meta );
+    if ( ! current_user_can( 'edit_post', $post_id ) ) {
+        return $post_id;
+    }
 
-    $body = array(
-        'post_type' => 'post',
-        'ID' => $post_id,
-        'data' => $data
-    );
+    if ( ! in_array( $_POST['post_type'], $index_post_types ) ) {
+		return $post_id;
+	}
 
-    $api_url = 'http://wpelastic.dev/app/plugins/wp-elastic-api';
-    \Httpful\Request::post( $api_url . '/posts' )->body( json_encode( $body ) )->send();
+	$data = array();
+
+	$post = get_object_vars( get_post( $post_id ) );
+	$data = array_merge( $data, $post );
+
+	$meta = get_post_meta( $post_id );
+	$data = array_merge( $data, $meta );
+
+	$body = array(
+		'post_type' => 'post',
+		'ID'        => $post_id,
+		'data'      => $data
+	);
+
+	\Httpful\Request::post( $api_url . '/posts' )->body( json_encode( $body ) )->send();
+
+	return $post_id;
 
 }
